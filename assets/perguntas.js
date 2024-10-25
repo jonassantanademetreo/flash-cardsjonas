f55adc819bdf1e964fb6c337c39614131e55901f
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2024-10-25 08:07:38 -0300

    25/10

---
+++ assets/perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+'conhecimentos gerais',
+'1. Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+'Japão e Serra Leoa'
+
+)
+criaCartao(
+    'conhecimentos gerais',
+    'Quais os principais autores do Barroco no Brasil?',
+    'Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira'
+    
+    )
+    criaCartao(
+        'conhecimentos gerais',
+        'Quem pintou "Guernica"?'
+        ' Pablo Picasso'
+        
+        )
+
+        criaCartao(
+                'conhecimentos gerais',
+            'Em que ordem surgiram os modelos atômicos?',
+            'Dalton, Thomson, Rutherford, Rutherford-Bohr'
+            
+            )
